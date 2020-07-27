5dec59c6f80e40d1faba3a723f046a55916d1f22
Xargin <buaa.cch@gmail.com>
2020-07-28 02:04:53 +0800

    dedup section articles

--- economist/section.go
+++ economist/section.go
@@ -22,7 +22,7 @@ func getSectionsAndCoverByURL(rootPath string) (sections []section, coverURL str
 	sectionCollector.OnHTML(".layout-weekly-edition-section", func(e *colly.HTMLElement) {
 		title := e.ChildText(".ds-section-headline")
 		children := e.ChildAttrs("a", "href")
-		sections = append(sections, section{title: strings.Trim(title, `'`), articleLinks: children})
+		sections = append(sections, section{title: strings.Trim(title, `'`), articleLinks: dedup(children)})
 	})
 	sectionCollector.OnHTML(".weekly-edition-header__image", func(e *colly.HTMLElement) {
 		coverURL = e.ChildAttr("img", "src")
@@ -34,4 +34,16 @@ func getSectionsAndCoverByURL(rootPath string) (sections []section, coverURL str
 	}
 
 	return sections, coverURL, title
+}
+
+func dedup(input []string) []string {
+	var res = []string{}
+	var rec = map[string]struct{}{}
+	for _, c := range input {
+		if _, ok := rec[c]; !ok {
+			res = append(res, c)
+			rec[c] = struct{}{}
+		}
+	}
+	return res
 }
